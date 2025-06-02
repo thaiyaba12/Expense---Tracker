86e746b31844fdd987c81ed4b7800efb2ff8aa0b
thaiyaba12 <tshaguftha@gmail.com>
2025-06-02 13:56:17 +0300

    changes made

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def add_expense():
 
 def view_expenses():
     if not expenses:
-     print("No expenses yet.\n")
-     return
+        print("No expenses yet.\n")
+        return
     for category, amount in expenses:
         print(f"{category} - ₹{amount}")
     print()
@@ -19,21 +19,21 @@ def view_total():
     print(f"Total spent: ₹{total}\n")
 
 while True:
-    print("1. Add Expense")
-    print("2. View Expenses")
-    print("3. View Total")
-    print("4. Exit")
+      print("1. Add Expense")
+      print("2. View Expenses")
+      print("3. View Total")
+      print("4. Exit")
 
-    choice = input("Enter your choice (1–4): ")
+      choice = input("Enter your choice (1–4): ")
 
-    if choice == "1":
-     add_expense()
-    elif choice == "2":
-     view_expenses()
-    elif choice == "3":
-     view_total()
-    elif choice == "4":
-     print("Goodbye!")
-     break
-    else:
+      if choice == "1":
+          add_expense()
+      elif choice == "2":
+          view_expenses()
+      elif choice == "3":
+          view_total()
+      elif choice == "4":
+         print("Goodbye!")
+         break
+      else:
         print("Invalid choice. Try again.\n")
